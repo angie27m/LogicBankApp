3383d717a2eefc08cd29e8359d7dd880d5e884b7
Angie <angie27manrique@gmail.com>
2023-05-13 22:58:08 -0500

    Initial commit

---
+++ src/main/java/com/neoris/pruebatecnicaneoris/PruebaTecnicaNeorisApplication.java
@@ -0,0 +1,13 @@
+package com.neoris.pruebatecnicaneoris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaNeorisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaTecnicaNeorisApplication.class, args);
+    }
+
+}
